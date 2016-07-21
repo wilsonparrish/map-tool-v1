9522c1f02ff468bfbe1e8609b3626b411aa3b391
Wilson Parrish <WParrish@vrx.local>
2016-07-21 14:59:24 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+MAP TOOL V1
+
+Using interact.js to create a simple drag and drop "tabletop" simulator map grid.
+
+log
+----
+07/21/2016 - Initial Commit
---
+++ snap.js
@@ -0,0 +1,40 @@
+var element = document.getElementById('grid-snap'),
+    x = 0, y = 0;
+
+interact(element)
+    .draggable({
+        snap: {
+            targets: [
+                interact.createSnapGrid({ x: 30, y: 30 })
+            ],
+            range: Infinity,
+            relativePoints: [{ x: 0, y: 0 }]
+        },
+        inertia: true,
+        restrict: {
+            restriction: element.parentNode,
+            elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+            endOnly: true
+        }
+    })
+    .on('dragmove', function (event) {
+        //this block keeps it in the lines when scrolled unevenly
+        if (event.dx % 30 >= 15) {    
+            event.dx += event.dx % 30
+        } else {
+            event.dx -= event.dx % 30
+        }
+        if (event.dy % 30 >= 15) {
+            event.dy += event.dy % 30
+        } else {
+            event.dy -= event.dy % 30
+        }
+
+        //handles the movement event
+        x += event.dx;
+        y += event.dy;
+        console.log(x, y);
+        event.target.style.webkitTransform =
+            event.target.style.transform =
+            'translate(' + x + 'px, ' + y + 'px)';
+    });

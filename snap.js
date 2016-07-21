2b7c3fc29e950e022216c3997312ea6fdc99bb9e
Wilson Parrish <WParrish@vrx.local>
2016-07-21 15:57:11 -0600

    added simple die roller

--- snap.js
+++ snap.js
@@ -1,7 +1,7 @@
-var element = document.getElementById('grid-snap'),
+var draggableElement = document.getElementById('grid-snap'),
     x = 0, y = 0;
 
-interact(element)
+interact(draggableElement)
     .draggable({
         snap: {
             targets: [
@@ -12,14 +12,14 @@ interact(element)
         },
         inertia: true,
         restrict: {
-            restriction: element.parentNode,
+            restriction: draggableElement.parentNode,
             elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
             endOnly: true
         }
     })
     .on('dragmove', function (event) {
         //this block keeps it in the lines when scrolled unevenly
-        if (event.dx % 30 >= 15) {    
+        if (event.dx % 30 >= 15) {
             event.dx += event.dx % 30
         } else {
             event.dx -= event.dx % 30
@@ -38,3 +38,27 @@ interact(element)
             event.target.style.transform =
             'translate(' + x + 'px, ' + y + 'px)';
     });
+
+var button = document.getElementById('roll-button');
+button.onclick = function (e) {
+    rollDice();
+}
+
+function rollDice() {
+    var numberOfDice = document.getElementById('dice-number').value;
+    var typeOfDice = document.getElementById('dice-type').value;
+    var dieRolls = [];
+    console.log('number', numberOfDice);
+    console.log('type', typeOfDice);
+    while (numberOfDice > 0) {
+        var dieRoll = rollRandom(typeOfDice);
+        console.log('roll ' + numberOfDice, dieRoll);
+        dieRolls.push(dieRoll);
+        numberOfDice--;
+    }
+    document.getElementById('roll').textContent = (dieRolls.reduce(function(x,y){return x+y;})).toString();
+};
+
+function rollRandom(dieNum) {
+    return Math.floor(Math.random() * dieNum) + 1;
+}

aaa3ffa412a5b704aba43a8c3c3882ed08b26038
VRX\WParrish <wparrish@myvrx.com>
2016-09-14 16:12:45 -0600

    further work on map, scrapping the ability to change size dynamically for now

--- scripts/controllers/divmapMakerCtrl.js
+++ scripts/controllers/divmapMakerCtrl.js
@@ -7,24 +7,51 @@
             $scope.cellsArray = [];
             $scope.mapWidth;
             $scope.mapHeight;
+            $scope.activeColor;
+            var cachedHeight;
+            var cachedWidth;
 
             $scope.createMap = function () {
                 $scope.mapWidth = $scope.mapDivWidth * 30 + 'px';
                 $scope.mapHeight = $scope.mapDivHeight * 30 + 'px';
+                $scope.cellsArray = [];
                 if ($scope.mapDivHeight > 0 && $scope.mapDivWidth > 0) {
                     $scope.rendering = true;
                     var mapSize = $scope.mapDivWidth * $scope.mapDivHeight;
-                    $scope.cellsArray = [];
-                    // if ($scope.cellsArray.length === 0) {
-                        for (var i = 0; i < mapSize; i++) {
-                            $scope.cellsArray.push({});
-                        }
-                    // }
+                    for (var i = 0; i < mapSize; i++) {
+                        $scope.cellsArray.push({});
+                    }
                     $timeout(function () {
                         $scope.rendering = false;
                     }, 1500);
                 }
             }
+
+            $scope.paintColorsArray = [
+                {
+                    background: 'red'
+                }, {
+                    background: 'purple'
+                }, {
+                    background: 'blue'
+                }, {
+                    background: 'green'
+                }, {
+                    background: 'yellow'
+                }, {
+                    background: 'orange'
+                }, {
+                    background: 'black'
+                }, {
+                    background: 'grey'
+                }, {
+                    background: 'white'
+                },
+            ];
+
+            $scope.applyColor = function (color) {
+                $scope.activeColor = color;
+            };
 
         })
 

b6641be98c883ef06537f9ed3d2150caaba1c80c
wpmk2 <wilsonparrishmk2@gmail.com>
2016-09-15 00:34:02 -0600

    able to add custom tiles now, need to do profiles next

--- scripts/controllers/divmapMakerCtrl.js
+++ scripts/controllers/divmapMakerCtrl.js
@@ -8,8 +8,8 @@
             $scope.mapWidth;
             $scope.mapHeight;
             $scope.activeColor;
-            var cachedHeight;
-            var cachedWidth;
+            $scope.activeCustom;
+            $scope.adding = false;
 
             $scope.createMap = function () {
                 $scope.mapWidth = $scope.mapDivWidth * 30 + 'px';
@@ -29,6 +29,20 @@
             
             $scope.applyColor = function (color) {
                 $scope.activeColor = color;
+                $scope.activeCustom = undefined;                
+            };
+
+            $scope.applyCustom = function (url) {
+                $scope.activeCustom = url;
+                $scope.activeColor = undefined;                
+            };
+
+            $scope.addCustom = function() {
+                $scope.customTiles.push({
+                    backgroundUrl: "'" + $scope.newUrl + "'"
+                })
+                $scope.newUrl = '';
+                
             };
 
             $scope.paintColorsArray = [
@@ -71,6 +85,18 @@
                 {
                     background: 'white'
                 },
+            ];
+
+            $scope.customTiles = [
+                {
+                    backgroundUrl: "https://s-media-cache-ak0.pinimg.com/236x/ea/ec/66/eaec665dea9111ac6029b11d65ff9e75.jpg"           
+                },
+                {
+                    backgroundUrl: "http://the-lost-and-the-damned.664610.n2.nabble.com/file/n7581201/10857.jpg"
+                },
+                {
+                    backgroundUrl: "http://www.dundjinni.com/forums/uploads/aegean/67Z_fractured_floor6_ae.png"
+                }
             ];
 
         })

bb6513164112823ec7f8af5af9674be2f3295b7a
VRX\WParrish <wparrish@myvrx.com>
2016-09-20 16:25:22 -0600

    removing a bunch of deprecated things, getting ready to add logbooks

--- scripts/app.routes.js
+++ scripts/app.routes.js
@@ -10,11 +10,6 @@
                     url: '/',
                     templateUrl: '../views/home.html'
                 })
-                .state('gridmap', {
-                    url: '/gridmap',
-                    templateUrl: '../views/gridmap.html',
-                    controller: 'mapCtrl'
-                })
                 .state('divmap', {
                     url: '/divmap',
                     templateUrl: '../views/divmap.html',

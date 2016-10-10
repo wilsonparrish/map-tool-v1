0096f659d46657c9675c9068c2969c08a00581f0
VRX\WParrish <wparrish@myvrx.com>
2016-10-10 15:00:38 -0600

    saved maps can now have their sizes edited without losing work

--- scripts/controllers/divmapMakerCtrl.js
+++ scripts/controllers/divmapMakerCtrl.js
@@ -6,7 +6,7 @@
 
             var fbref = firebase.database().ref();
             var mapBeingEdited;
-            
+
             $scope.user = JSON.parse(localStorage.getItem('firebaseUser'));
 
             $scope.cellsArray = [];
@@ -43,7 +43,7 @@
                             tooltip: ""
                         });
                     }
-                    $scope.updatingMap = false;                    
+                    $scope.updatingMap = false;
                     $timeout(function () {
                         $scope.rendering = false;
                     }, 500);
@@ -68,6 +68,10 @@
             $scope.savedMaps = mapsService.getMaps();
 
             $scope.saveMap = function (mapName) {
+                if (!$scope.mapName) {
+                    alert("Map name must be specified");
+                    return;
+                }
                 var mapData = {
                     map: JSON.stringify($scope.cellsArray),
                     height: $scope.mapDivHeight,
@@ -78,6 +82,12 @@
                 };
                 console.log('saving: ', mapData);
                 if (mapName) {
+                    // for (var i = 0; i < $scope.savedMaps.length; i++) {
+                    //     if (mapName === $scope.savedMaps[i].name) {
+                    //         alert("A map by that name already exists");
+                    //         return;
+                    //     }
+                    // }
                     for (var i = 0; i < $scope.savedMaps.length; i++) {
                         if ($scope.savedMaps[i].name === mapName) {
                             console.log('saving map at index ' + i + ' with an $id of ' + $scope.savedMaps[i].$id + ' which should be equal to ' + $scope.savedMaps.$keyAt(i));
@@ -89,20 +99,122 @@
                             $scope.savedMaps[i].owner = $scope.user.user.displayName;
                             $scope.savedMaps.$save(i).then(function (ref) {
                                 console.log('saved: ', ref);
+                                toastr.success('Map saved successfully', 'Success');
                             }, function (err) {
                                 // alert("there was an error saving this map", err);
                                 console.log(err);
+                                toastr.error('An error occurred', 'Error');                                
                             })
                         }
                     }
                 } else {
                     $scope.savedMaps.$add(mapData).then(function (ref) {
+                        toastr.success('Map saved successfully', 'Success');                        
                         console.log('saved: ', ref);
                     }, function (err) {
-                        // alert("there was an error saving this map", err);
+                        toastr.error('An error occurred', 'Error');                                
                         console.log(err);
                     })
                 }
+            }
+
+            // Resizing maps 
+            $scope.resizeOptions = [
+                {
+                    display: 'Add rows to top',
+                    optionNum: 1
+                }, {
+                    display: 'Add rows to bottom',
+                    optionNum: 2
+                }, {
+                    display: 'Add colums to left',
+                    optionNum: 3
+                }, {
+                    display: 'Add columns to right',
+                    optionNum: 4
+                }, {
+                    display: 'Remove rows from top',
+                    optionNum: 5
+                }, {
+                    display: 'Remove rows from bottom',
+                    optionNum: 6
+                }, {
+                    display: 'Remove columns from left',
+                    optionNum: 7
+                }, {
+                    display: 'Remove columns from right',
+                    optionNum: 8
+                },
+            ];
+
+            $scope.resizeRowsCols = 0;
+
+            $scope.resizeMap = function () {
+                if (!$scope.mapName) {
+                    alert("Map must be saved before resizing.");
+                    return;
+                }
+                console.log($scope.chosenResizeOption);
+                switch ($scope.chosenResizeOption) {
+                    case 1:
+                        var cellsToAdd = $scope.resizeRowsCols * $scope.mapDivWidth;
+                        for (var i = 0; i < cellsToAdd; i++) {
+                            $scope.cellsArray.unshift({ tooltip: "" });
+                        }
+                        $scope.mapDivHeight += $scope.resizeRowsCols;
+                        break;
+                    case 2:
+                        var cellsToAdd = $scope.resizeRowsCols * $scope.mapDivWidth;
+                        for (var i = 0; i < cellsToAdd; i++) {
+                            $scope.cellsArray.push({ tooltip: "" });
+                        }
+                        $scope.mapDivHeight += $scope.resizeRowsCols;
+                        break;                        
+                    case 3:
+                        $scope.mapDivWidth += $scope.resizeRowsCols;                                                
+                        for (var i = 0; i < $scope.mapDivHeight; i++){
+                            for (var j = 0; j < $scope.resizeRowsCols; j++){
+                                $scope.cellsArray.splice($scope.mapDivWidth * i,0,{ tooltip: "" });                                
+                            }
+                        }
+                        break;
+                    case 4:
+                        $scope.mapDivWidth += $scope.resizeRowsCols;                                                
+                        for (var i = 1; i <= $scope.mapDivHeight; i++){
+                            for (var j = 0; j < $scope.resizeRowsCols; j++){
+                                $scope.cellsArray.splice($scope.mapDivWidth * i,0,{ tooltip: "" });                                
+                            }
+                        }
+                        break;                        
+                    case 5:
+                        var cellsToRemove = $scope.resizeRowsCols * $scope.mapDivWidth;
+                        for (var i = 0; i < cellsToRemove; i++) {
+                            $scope.cellsArray.shift();
+                        }
+                        $scope.mapDivHeight -= $scope.resizeRowsCols;
+                        break;                        
+                    case 6:
+                        var cellsToRemove = $scope.resizeRowsCols * $scope.mapDivWidth;
+                        for (var i = 0; i < cellsToRemove; i++) {
+                            $scope.cellsArray.pop();
+                        }
+                        $scope.mapDivHeight -= $scope.resizeRowsCols;
+                        break;                        
+                    case 7:
+                        $scope.mapDivWidth -= $scope.resizeRowsCols;
+                        for (var i = 0; i < $scope.mapDivHeight; i++){
+                            $scope.cellsArray.splice($scope.mapDivWidth * i, $scope.resizeRowsCols);                                
+                        }                                                
+                        break;
+                    case 8:
+                        $scope.mapDivWidth -= $scope.resizeRowsCols;                        
+                        for (var i = 1; i <= $scope.mapDivHeight; i++){
+                            $scope.cellsArray.splice($scope.mapDivWidth * i, $scope.resizeRowsCols);                                
+                        }
+                        break;
+                }
+                $scope.saveMap($scope.mapName);
+                $scope.createMap(false, $scope.chosenMap);
             }
 
 
